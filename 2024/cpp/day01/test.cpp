c29bd66e012fe425f1269c1d85b9b2f80bbfd473
Kwamena A <155214889+kawotwi@users.noreply.github.com>
2025-07-31 19:03:32 -0400

    Initialized folders and day01 with hello world messages. Included linters and some initial pip related requirements for python and C++.

---
+++ 2024/cpp/day01/test.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Testing hello world here." << endl;
+    return 0;
+}
---
+++ 2024/python/day01/test.py
@@ -0,0 +1,5 @@
+def main():
+    print("Testing hello world here.")
+
+if __name__ == "__main__":
+    main()
